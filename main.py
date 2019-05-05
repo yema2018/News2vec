a0d5e7da2228621f664b0856a0e3695ff2de824c
yema <ye.ma@xjtlu.edu.cn>
2019-05-05 16:06:48 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,102 @@
+import argparse
+import numpy as np
+import networkx as nx
+import node2vec
+from news2vec import newsfeature2vec
+
+def parse_args():
+	'''
+	Parses the node2vec arguments.
+	'''
+	parser = argparse.ArgumentParser(description="Run node2vec.")
+
+	parser.add_argument('--input', nargs='?', default='graph/8500_cut.edgelist',
+						help='Input graph path')
+
+	parser.add_argument('--output', nargs='?', default='emb/8500.emb',
+						help='Embeddings path')
+
+	parser.add_argument('--map', nargs='?', default='map/8500.map',
+						help='Map indice to nodes')
+
+	parser.add_argument('--include', nargs='?', default=True,
+						help='Boolean including keyword nodes')
+
+	parser.add_argument('--dimensions', type=int, default=128,
+						help='Number of dimensions. Default is 128.')
+
+	parser.add_argument('--walk-length', type=int, default=100,
+						help='Length of walk per source. Default is 100.')
+
+	parser.add_argument('--num-walks', type=int, default=5,
+						help='Number of walks per source. Default is 5.')
+
+	parser.add_argument('--window-size', type=int, default=10,
+						help='Context size for optimization. Default is 10.')
+
+	parser.add_argument('--p', type=float, default=1,
+						help='Return hyperparameter. Default is 1.')
+
+	parser.add_argument('--q', type=float, default=1,
+						help='Inout hyperparameter. Default is 1.')
+
+	parser.add_argument('--weighted', dest='weighted', action='store_true',
+						help='Boolean specifying (un)weighted. Default is unweighted.')
+	parser.add_argument('--unweighted', dest='unweighted', action='store_false')
+	parser.set_defaults(weighted=True)
+
+	parser.add_argument('--directed', dest='directed', action='store_true',
+						help='Graph is (un)directed. Default is undirected.')
+	parser.add_argument('--undirected', dest='undirected', action='store_false')
+	parser.set_defaults(directed=False)
+
+	return parser.parse_args()
+
+def read_graph():
+	'''
+	Reads the input network in networkx.
+	'''
+	if args.weighted:
+		G = nx.read_edgelist(args.input, nodetype=int, data=(('weight',float),), create_using=nx.DiGraph())
+	else:
+		G = nx.read_edgelist(args.input, nodetype=int, create_using=nx.DiGraph())
+		for edge in G.edges():
+			G[edge[0]][edge[1]]['weight'] = 1
+
+	if not args.directed:
+		G = G.to_undirected()
+
+	return G
+
+def learn_embeddings(walks):
+	'''
+	Learn embeddings by optimizing the Skipgram objective using SGD.
+	'''
+
+	map_dict = {}
+	with open(args.map, 'r',encoding='utf-8') as f:
+		for l in f:
+			l = l.strip('\n').split(' ')
+			map_dict[l[0]] = l[1]
+
+	walks1 = list()
+	for walk in walks:
+		walks1 += list(map(lambda x: map_dict[str(x)], walk))
+	print(walks1[:100])
+	newsfeature2vec(walks1,args.output,include=args.include,skip_window=args.window_size,iter=int(len(walks1)))
+	return
+
+def main(args):
+	'''
+	Pipeline for representational learning for all nodes in a graph.
+	'''
+	nx_G = read_graph()
+	G = node2vec.Graph(nx_G, args.directed, args.p, args.q)
+	G.preprocess_transition_probs()
+	walks = G.simulate_walks(args.num_walks, args.walk_length)
+	print(len(walks),walks[0])
+	learn_embeddings(walks)
+
+if __name__ == "__main__":
+	args = parse_args()
+	main(args)
---
+++ news2vec.py
@@ -0,0 +1,148 @@
+#!usr/bin/env python
+# -*- coding:utf-8 -*-
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '-1'
+import collections
+import math
+import random
+import numpy as np
+from six.moves import xrange
+import tensorflow as tf
+import pandas as pd
+
+
+
+class newsfeature2vec:
+    def __init__(self, walks, out_dir, batch_size=1, embedding_size=128, skip_window=5, num_skips=1, neg_samples=5, include=True, iter=3000000):
+        self.iter = iter
+        self.data_index = 0
+        if not include:
+            self.words = [i for i in walks if ',' in i]
+        else:
+            self.words = walks
+        self.batch_size = batch_size
+        self.embedding_size = embedding_size
+        self.skip_window = skip_window
+        self.num_skips = num_skips
+        self.neg_samples = neg_samples
+        self.build_dataset()
+        self.build_dataset_emb()
+        self.build_model()
+
+        with tf.Session(graph=self.graph) as session:
+            session.run(tf.global_variables_initializer())
+            print("Initialized")
+            average_loss = 0
+            for step in xrange(self.iter):
+                batch, labels = self.generate_batch()
+                for i in range(self.batch_size):
+                    word = self.reverse_dictionary[batch[i]]
+                    if ',' in word:
+                        inputs = np.array([self.dictionary_emb[i] for i in word.split(',')], dtype=int)
+                    else:
+                        inputs = np.array(self.dictionary_emb[word], dtype=int)
+                    feed_dict = {self.train_inputs: inputs.reshape(1,-1), self.train_labels: labels[i].reshape(1,1)}
+
+                    _, loss_val= session.run([self.optimizer, self.loss], feed_dict=feed_dict)
+                    average_loss += loss_val
+
+                if step % 2000 == 0:
+                    if step > 0:
+                        average_loss /= (2000*self.batch_size)
+                    # The average loss is an estimate of the loss over the last 2000 batches.
+                    print("Average loss at step ", step, ": ", average_loss)
+                    average_loss = 0
+
+            final = self.normalized_embeddings.eval()
+            pd.DataFrame(final, index=[i[0] for i in self.count_emb]).to_csv(out_dir)
+
+    def build_dataset(self):
+        self.count = []
+        self.count.extend([list(item) for item in collections.Counter(self.words).most_common()])
+        self.vocabulary_size = len(self.count)
+        self.dictionary = dict()
+        for word, _ in self.count:
+            self.dictionary[word] = len(self.dictionary)
+        self.data = list()
+        del self.count
+        for word in self.words:
+            if word in self.dictionary:
+                index = self.dictionary[word]
+            else:
+                index = 0
+            self.data.append(index)
+        self.reverse_dictionary = dict(zip(self.dictionary.values(), self.dictionary.keys()))
+
+    def build_dataset_emb(self):
+        self.words_emb = []
+        for word in [word.split(',') for word in self.words]:
+            self.words_emb += word
+        self.count_emb = []
+        self.count_emb.extend([list(item) for item in collections.Counter(self.words_emb).most_common()])
+        self.vocabulary_size_emb = len(self.count_emb)
+        self.dictionary_emb = dict()
+        for word, _ in self.count_emb:
+            self.dictionary_emb[word] = len(self.dictionary_emb)
+
+    # Function to generate a training batch for the skip-gram model.
+    def generate_batch(self):
+        assert self.batch_size % self.num_skips == 0
+        assert self.num_skips <= 2 * self.skip_window
+        batch = np.ndarray(shape=[self.batch_size], dtype=np.int32)
+        labels = np.ndarray(shape=(self.batch_size, 1), dtype=np.int32)
+        span = 2 * self.skip_window + 1  # [ skip_window target skip_window ]
+        buffer = collections.deque(maxlen=span)
+        for _ in range(span):
+            buffer.append(self.data[self.data_index])
+            self.data_index = (self.data_index + 1) % len(self.data)
+        for i in range(self.batch_size // self.num_skips):
+            target = self.skip_window  # target label at the center of the buffer
+            targets_to_avoid = [self.skip_window]
+            for j in range(self.num_skips):
+                while target in targets_to_avoid:
+                    target = random.randint(0, span - 1)
+                targets_to_avoid.append(target)
+                batch[i * self.num_skips + j] = buffer[self.skip_window]
+                labels[i * self.num_skips + j, 0] = buffer[target]
+            buffer.append(self.data[self.data_index])
+            self.data_index = (self.data_index + 1) % len(self.data)
+        return batch, labels
+
+    def build_model(self):
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            # Input data.
+            self.train_inputs = tf.placeholder(tf.int32, shape=[1,None])
+            self.train_labels = tf.placeholder(tf.int32, shape=[1, 1])
+
+            emb = tf.random_uniform([self.vocabulary_size_emb, self.embedding_size], -1.0, 1.0)
+
+            # Look up embeddings for inputs.
+            self.embeddings = tf.Variable(initial_value=emb, trainable=True)
+            embed=tf.reduce_sum(tf.nn.embedding_lookup(self.embeddings, self.train_inputs),axis=1)
+
+            nce_weights = tf.Variable(
+                tf.truncated_normal([self.vocabulary_size, self.embedding_size],
+                                    stddev=1.0 / math.sqrt(self.embedding_size)))
+            nce_biases = tf.Variable(tf.zeros([self.vocabulary_size]),dtype=tf.float32)
+
+            self.loss = tf.reduce_mean(tf.nn.nce_loss(weights=nce_weights,
+                                                 biases=nce_biases,
+                                                 inputs=embed,
+                                                 labels=self.train_labels,
+                                                 num_sampled=self.neg_samples,
+                                                 num_classes=self.vocabulary_size))
+
+            self.global_step = tf.Variable(0, name='global_step', trainable=False)
+            lr = tf.train.exponential_decay(0.01, global_step=self.global_step, decay_steps=int(0.05*self.iter),
+                                            decay_rate=0.95)
+            self.optimizer = tf.train.GradientDescentOptimizer(lr).minimize(self.loss, global_step=self.global_step)
+
+            norm = tf.sqrt(tf.reduce_sum(tf.square(self.embeddings), 1, keep_dims=True))
+            self.normalized_embeddings = self.embeddings / norm
+
+
